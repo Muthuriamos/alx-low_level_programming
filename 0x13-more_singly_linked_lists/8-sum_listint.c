93428606226900fc490e7c6e186634a5b7296eeb
Muthuriamos <amosmuthuuri@gmail.com>
2023-08-29 21:11:06 +0300

    8. Sum list

--- 0x13-more_singly_linked_lists/8-sum_listint.c
+++ 0x13-more_singly_linked_lists/8-sum_listint.c
@@ -9,11 +9,11 @@ int sum_listint(listint_t *head)
 	int sum = 0;
 	listint_t *current_node = head;
 
-	while (current_node != NULL)
+	while (current_node)
 	{
 		sum += current_node->n;
 		current_node = current_node->next;
 	}
 
-	return sum;
+	return (sum);
 }
