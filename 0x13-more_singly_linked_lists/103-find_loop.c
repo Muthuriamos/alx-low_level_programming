c50bb234c6340205f43061ad376cf9a6aef28381
Muthuriamos <amosmuthuuri@gmail.com>
2023-08-29 16:28:39 +0300

    14. Find the loop

--- 0x13-more_singly_linked_lists/103-find_loop.c
+++ 0x13-more_singly_linked_lists/103-find_loop.c
@@ -10,22 +10,27 @@ listint_t *find_listint_loop(listint_t *head)
 {
 	listint_t *slow, *fast;
 
+	slow = head;
+	fast = head;
+
 	while (slow && fast && fast->next)
 	{
 		slow = slow->next;
 		fast = fast->next->next;
+
 		if (slow == fast)
 		{
 			slow = head;
-			break;
+
+			while (slow != fast)
+			{
+				slow = slow->next;
+				fast = fast->next;
+			}
+
+			return (fast);
 		}
 	}
-	if (!slow || !fast || !fast->next)
-		return (NULL);
-	while (slow != fast)
-	{
-		slow = slow->next;
-		fast = fast->next;
-	}
-	return (fast);
+
+	return (NULL);
 }
