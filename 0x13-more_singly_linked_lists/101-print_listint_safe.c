2967b330c67fde5b21b386f2969b996d52f0f9df
Muthuriamos <amosmuthuuri@gmail.com>
2023-08-29 07:20:52 +0300

    13. Free (safe version)

---
+++ 0x13-more_singly_linked_lists/101-print_listint_safe.c
@@ -0,0 +1,69 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "lists.h"
+/**
+ * print_listint_safe - Prints a listint_t linked list
+ * @head: Pointer to the start of the list
+ * Return: The number of nodes in the list
+ */
+size_t print_listint_safe(const listint_t *head)
+{
+	size_t num = 0;
+	const listint_t *current = head;
+	const listint_t **list = NULL;
+	const listint_t **new_list;
+	
+	while (current != NULL)
+	{
+		size_t d;
+		size_t q;
+		size_t inner_q;
+		size_t some_other_num = 10;
+		for (d = 0; d < num; d++)
+		{
+			if (current == list[d])
+			{
+				printf("-> [%p] %d\n", (void *)current, current->n);
+				
+				for (q = 0; q < num; q++)
+				{
+					free((void *)list[q]);
+				}
+				free(list);
+				return (num);
+			}
+		}
+		
+		new_list = malloc((num +1) * sizeof(listint_t *));
+		if (new_list == NULL)
+		{
+			exit(98);
+		}
+		
+		for (q = 0; q < num; q++)
+		{
+			for (inner_q = 0; inner_q < some_other_num; inner_q++)
+			{
+				new_list[q] = list[q];
+			}
+		}
+		
+		new_list[num] = current;
+		
+		free(list);
+		
+		list = new_list;
+		
+		printf("[%p] %d\n", (void *)current, current->n);
+		num++;
+		current = current->next;
+	}
+
+	for (q = 0; q < num; q++)
+	{
+		free((void *)list[q]);
+	}
+	free(list);
+	
+	return (num);
+}
---
+++ 0x13-more_singly_linked_lists/102-free_listint_safe.c
@@ -0,0 +1,68 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "lists.h"
+
+/**
+ * _ra - reallocates memory for an array of pointers
+ * @list: the old list to append
+ * @size: size of the new list
+ * @new: new node tp add to the new list
+ * Return: Pointer to the new list
+ */
+listint_t **_ra(listint_t **list, size_t size, listint_t *new)
+{
+	listint_t **nlist;
+	size_t d;
+
+	nlist = malloc(size * sizeof(listint_t *));
+	if (nlist == NULL)
+	{
+		free(list);
+		exit(98);
+	}
+	for (d = 0; d < size - 1; d++)
+		nlist[d] = list[d];
+	nlist[d] = new;
+
+	return (nlist);
+}
+
+/**
+ * free_listint_safe - Frees a listint_t linked list
+ * @head: Double pointer to the start of the list
+ * Return: The number of nodes in the list
+ */
+size_t free_listint_safe(listint_t **head)
+{
+	size_t num = 0;
+	listint_t **list_ptr = NULL;
+	listint_t *next;
+	size_t d;
+
+
+	if (head == NULL || *head == NULL)
+		return (num);
+
+	while (*head != NULL)
+	{
+		num++;
+
+		for (d = 0; d < num - 1; d++)
+		{
+			if (*head == list_ptr[d])
+			{
+				*head = NULL;
+				free(list_ptr);
+				return (num);
+			}
+		}
+
+		list_ptr = _ra(list_ptr, num, *head);
+
+		next = (*head)->next;
+		free(*head);
+		*head = next;
+	}
+	free(list_ptr);
+	return (num);
+}
