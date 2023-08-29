a0ca209759d2a41edc0c0ffca63d680e6519e5d4
Muthuriamos <amosmuthuuri@gmail.com>
2023-08-29 15:40:47 +0300

    2. Add node

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -10,38 +10,32 @@
  */
 list_t *add_node_end(list_t **head, const char *str)
 {
-	list_t *new_node, *temp;
+	list_t *new_node;
+	size_t str_len = 0;
 
 	if (str == NULL)
+	{
 		return (NULL);
+	}
+
+	while (str[str_len] != '\0')
+		str_len++;
 
 	new_node = malloc(sizeof(list_t));
+
 	if (new_node == NULL)
 		return (NULL);
 
-	new_node->str = strup(str);
+	new_node->str = strdup(str);
 	if (new_node->str == NULL)
 	{
 		free(new_node);
 		return (NULL);
 	}
 
-	new_node->len = strlen(str);
+	new_node->len = str_len;
+	new_node->next = *head;
+	*head = new_node;
 
-	new_node->next = NULL;
-
-	if (*head == NULL)
-	{
-		*head = new_node;
-	}
-	else
-	{
-		temp = *head;
-		while (temp->next != NULL)
-			temp = temp->next;
-
-		temp->next = new_node;
-	}
-
-	return (new_node);
+	return (*head);
 }
