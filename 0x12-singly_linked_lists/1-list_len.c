0792fa3e40cec3a741e0ab74334074e596f6efa7
Muthuriamos <amosmuthuuri@gmail.com>
2023-08-29 12:24:47 +0300

    1. List length

--- 0x12-singly_linked_lists/1-list_len.c
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -3,18 +3,17 @@
 
 /**
  * list_len - gets the number of nodes
- * @h: pointer to the head
- * Return: number of nodes
+ * @h: Pointer to the head of the list
+ * Return: Number of nodes
  */
 size_t list_len(const list_t *h)
 {
-	size_t count = 0;
-	const list_t *temp = h;
+	size_t n = 0;
 
-	while (temp != NULL)
+	while (h != NULL)
 	{
-		count++;
-		temp = temp->next;
+		n++;
+		h = h->next;
 	}
-	return (count);
+	return (n);
 }
