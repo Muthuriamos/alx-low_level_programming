bc15a5eaeb3727c7b90207388c36f36fc9c5d5f9
Muthuriamos <amosmuthuuri@gmail.com>
2023-08-31 19:58:30 +0300

    1. 1

---
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * get_bit -Returns the value of a bit at a given index
+ * @n: Checking bits
+ * @index: Which to check bit
+ * Return: The value of the bit at the given ondex or -1
+ */
+int get_bit(unsigned long int n, unsigned int index)
+{
+	unsigned long int mask, mist;
+
+	if (index > (sizeof(unsigned long int) * 8 - 1))
+		return (-1);
+
+	mask = 1 << index;
+	mist = n & mask;
+
+	if (mist == mask)
+		return (1);
+
+	return (0);
+}
+
+
